b34d7a72df8d2058ac12c5f9db4a3f44ad6d61a5
Joey Gaitan <joeygaitan707@gmail.com>
2018-10-10 15:31:34 -0700

    constructor finished for page. I created the object that will store the values for the array

--- README.md
+++ README.md
@@ -9,4 +9,15 @@ monday (October 8th 2018) plan
 ---------------------------------
 Actually done (October 8th 2018)
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
+-I finished up the template for the main page.
+-Almost finished the intial design for the template.
+--------------------------------
+Tuesday (October 9th 2018) plan
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-link the template page with the main page
+-finish the editable webpage
+-finish the forms for the template html page
+-------------------------------
+Actually done (October 9th 2018)
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+-
---
+++ js/main.js
@@ -0,0 +1,38 @@
+var objectHeader = [{
+    TabHeader: "",
+    fontSelector: "",
+    fontColor: "",
+    firstLastName: "",
+    location: "",
+    personalImage:"",
+    jobtitle: "",
+    backgroundColorImage: "" 
+}];
+
+var objectContact =[{
+    font: "",
+    fColor: "",
+    Phone: "",
+    email: "",
+    phone: "",
+    git: "",
+    linkedn: "",
+}]
+
+var objectBody = [{
+    fontselct: "",
+    fontcol: "",
+    languages: "",
+    Educaion: "",
+    Experience: ""
+}];
+
+var objectFooter = [{
+    fontSEL: "",
+    fontCO: "",
+    backgroundColor: "",
+    Date: ""
+}];
+
+const primer = document.querySelector('')
+
