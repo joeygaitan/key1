d375b4174b4277be0cda190563e41c0822525574
Joey Gaitan <joeygaitan707@gmail.com>
2018-10-12 10:07:54 -0700

    Finished project. Got it to shoot out the code and only input the proper data

--- js/save.js
+++ js/save.js
@@ -1,11 +1,9 @@
 const file = localStorage.getItem('Html')
+if(file){
+    document.querySelector("#past").textContent = file.replace(/\\n/g,'').replace(/\\t/g,'').replace(/\\"/g,'"');
+    
 
-if(!file){
-
-}else{
-    document.querySelector("past").innerContent = file;
-
-    document.querySelector('press').addEventListener('click', function(event){
+    document.querySelector('#press').addEventListener('click', function(event){
         event.preventDefault()
 
         function sam(){
